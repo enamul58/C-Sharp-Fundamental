fd92c3147613bd1fb68484b555a10b83e77e3e5b
“enamul58” <enamullimon@gmail.com>
2024-05-04 15:25:43 +0600

    first commit

---
+++ EvenNumberShow/EvenNumberShow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+           int sum = p.Add(20, 20);
+           Console.WriteLine(sum);
+           
+            Program.EvenNumber(30);
+            Console.ReadKey();
+        }
+
+        public int Add(int num1, int num2 )
+        {
+            return num1 + num2;
+        }
+       
+        public static void EvenNumber(int range)
+        {
+            int start = 0;
+            while( start <= range)
+            {
+                Console.WriteLine(start);
+                start = start + 2; 
+            }
+        }
+
+    }
+
