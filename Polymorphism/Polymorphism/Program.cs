b60017aff23b30f550719b78ff83cf3862bf60e1
“enamul58” <enamullimon@gmail.com>
2024-05-11 12:50:53 +0600

    polymorphism

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Employee
+    {
+        public string FirstName = "Md.Enamul Haque";
+        public string LastName = "Sarker";
+
+        public void FullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        public void FullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "Part time employee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employee = new Employee[2];
+            employee[0] = new Employee();
+            employee[1] = new PartTimeEmployee();
+
+            foreach(Employee emp in employee)
+            {
+                Console.WriteLine("loop");
+                emp.FullName();
+            }
+            Console.ReadKey();
+        }
+    }
+}
